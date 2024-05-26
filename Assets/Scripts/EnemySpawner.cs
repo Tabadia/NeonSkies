8835e66be786199b56a4f67d51e527f8cc6c2f2a
Thalen <thalenabadia@gmail.com>
2024-05-25 18:06:46 -0700

    bomber spawning fixed

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,22 +7,27 @@ public class EnemySpawner : MonoBehaviour
     public float spawnFrequency = 10f;
     public float minHeight = 5f;
     public float maxHeight = 10f;
+    public GameObject player;
+    public float farAwayX = 15f;
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(SpawnBombers());
     }
 
     private IEnumerator SpawnBombers()
-{
-    while (true)
     {
-        float spawnHeight = Random.Range(Screen.height * 1 / 2, Screen.height * 11/12);
-        float spawnEdge = Random.value < 0.5f ? 0 : Screen.width;
-        Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(spawnEdge, spawnHeight, Camera.main.nearClipPlane));
-        GameObject bomber = Instantiate(bomberPrefab, spawnPosition, Quaternion.identity);
-        bomber.GetComponent<Bomber>().direction = spawnEdge == 0 ? Vector3.right : Vector3.left;
-        yield return new WaitForSeconds(spawnFrequency);
+        while (true)
+        {
+            float spawnHeight = player.transform.position.y + Random.Range(minHeight, maxHeight);
+            float spawnEdge = player.transform.position.x + (Random.value < 0.5f ? -farAwayX : farAwayX);
+            Vector3 spawnPosition = new Vector3(spawnEdge, spawnHeight, 0);
+            Vector3 direction = spawnEdge < player.transform.position.x ? Vector3.right : Vector3.left;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            GameObject bomber = Instantiate(bomberPrefab, spawnPosition, Quaternion.Euler(0, 0, -angle));
+            bomber.GetComponent<Bomber>().direction = direction;
+            yield return new WaitForSeconds(spawnFrequency);
+        }
     }
-}
 }
